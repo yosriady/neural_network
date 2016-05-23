d233cbe17ba157492c6d5c656496e58b15cb694f
Yos Riady <yosriady@gmail.com>
2016-05-23 18:21:29 +0800

    Initial commit

---
+++ lib/neural_network.ex
@@ -0,0 +1,34 @@
+defmodule NeuralNetwork do
+  @moduledoc """
+    This module implements a feed-forward artifical neural network with backpropagation.
+  """
+
+  defstruct []
+
+  @type t :: %NeuralNetwork{
+
+  }
+
+  @doc """
+    Creates a new neural network.
+    Accepts optional arguments such as error treshold, iterations, and learning rate.
+  """
+  def new(options \\ []) do
+    # TODO
+  end
+
+  @doc """
+    Trains a neural network.
+  """
+  def train([]) do
+    # TODO
+  end
+
+  @doc """
+    Traverse the network for a given input and return a result.
+  """
+  def run([]) do
+    # TODO
+  end
+
+end
---
+++ lib/neural_network/math.ex
@@ -0,0 +1,17 @@
+defmodule NeuralNetwork.Math do
+  @moduledoc """
+    This module implements some math-related utility functions.
+  """
+
+  @spec sigmoid(float) :: float
+  def sigmoid(z) do
+      1 / (1 + :math.pow(e, -z))
+  end
+
+  @doc """
+    The mathematical constant e.
+  """
+  @spec e :: float
+  def e, do: 2.718281828459045
+
+end
